17c092975dde3cf5d9c2af1bf1df8e2b7e9cd880
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-05-01 16:43:49 +0530

    css folder

--- Book-Bus-Tickets-in-affordable-prices-in-Chattisgarh.php
+++ Book-Bus-Tickets-in-affordable-prices-in-Chattisgarh.php
@@ -17,7 +17,7 @@ if (isset($_POST['submit'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>New Shamim Transport</title>
-  <link rel="stylesheet" href="new/nav.css" />
+  <link rel="stylesheet" href="./css/nav.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" />
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
